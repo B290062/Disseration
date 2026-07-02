ebffe056845cbc36d6ee9b9eb981b37fa876a8fc
Caleb Brown <s2089123@bioinfmsc8.bio.ed.ac.uk>
2026-07-02 15:19:51 +0100

    mad the trimming function more uniform and added comments the to the previous functions. ALSO gro-seq functionality for trimming

--- Pipeline.py
+++ Pipeline.py
@@ -14,14 +14,15 @@ print("# For usage information please type --help in the command terminal#")
 print("################################################################### ")
 
 config = configparser.ConfigParser()
-config.read("config2.ini")
+config.read("config.ini")
 
+#The following are command line arguments that can be specified in the console by the user, each of which denotes its use.
 parser = argparse.ArgumentParser()
 parser.add_argument("--sra", action="store", required=True, help="This is the SRA number from Gene Expression eg for GSE87821 it would be SRP091444)")
 #store_true used for optional values, when the value is present it will be True so this can be used to write functions.
 #https://docs.python.org/3/library/argparse.html#action
 parser.add_argument("--trim", action="store_true", required=False, help = "The data can be optionally trimmed if the user requires")
-parser.add_argument("--adapter1",required=False,help="The first adapter required for trimming")
+parser.add_argument("--adapter1",required=False,help="The first adapter required for trimming. GRO-seq only requires one adapter.")
 parser.add_argument("--adapter2",required=False, help="The second adapter required for trimming") 
 parser.add_argument("--multiqc", action="store_true", required=False, help = "This argument enables the use of MultiQC")
 #Function to download fastq files from SRA 
@@ -36,15 +37,12 @@ def SRA_download(args):
     #replaced with makedirs instead of os.mkdirs as it has the exist_ok function which prevents crashing
     #when the folder is already created.
     os.makedirs("SRA", exist_ok=True)
-    print('A Directory called SRA was created.')
-    print('-----------------')
+    #moves into the created/existing SRA directory
     os.chdir("SRA")
     print('Attempting to download fastq files from GEO')
-    print('----------------------------------------------------')
     #if statement adapted from https://www.geeksforgeeks.org/python/check-if-directory-contains-file-using-python/
     #this checks if the current directory "SRA" contains one or more files and skips the download process if true
     #essential as the SRA files for any given study are very large.
-    #this could also be replaced with a download skip argument that runs main without the SRA, FASTA, and GTF download steps. implement later.
     if len(os.listdir(".")) > 0:
         print("SRA files were detected, inside the SRA directory, therefore the download will be skipped")
         os.chdir("..")
@@ -55,30 +53,19 @@ def SRA_download(args):
         print('Error occured during download of fastQ files')
         exit(1)
     else: 
-        print('Download completed')
-        print('------------------')
+        print('Fastq file download successful, storing in the SRA directory.')
         time.sleep(0.5)
+        #leaves the SRA directory backs into the main directory.
         os.chdir("..")
 
 def Quality_control(args):
     #produces FastQC files which allow the user to examine to determine the quality of the data.
     print('Beginning performing quality control...')
-    print('---------------------------------')
-    
-    time.sleep(0.5)
-    print('Creating new directory for FASTQC files...')
-    print('----------------------------------')
     time.sleep(0.5)
     os.makedirs("FastQC", exist_ok = True)
     print("FastQC directory created")
-    print('--------')
-
-
-    #running FastQC
-
-    print('Running FastQC....')
-    time.sleep(0.5)
-    print('---------------')
+    #this code appears in all of the functions and allows for time to be saved, if files are detected.
+    #only portions of the pipeline that need to be ran are ran.
     if len(os.listdir("FastQC")) > 0:
         print("FastQC files detected in the folder, skipping this step")
         return
@@ -86,16 +73,16 @@ def Quality_control(args):
     fastqc_run = subprocess.run('fastqc SRA/*.fastq -o FastQC', shell=True)
     if fastqc_run.returncode !=0:
         print('FastQC were not able to be produced')
+        exit(1)
     else:
         print('Finished FastQC analysis')
-        print('-------------------------')
         time.sleep(0.5)
         print('All the files are in the fastqc directory')
-        print('---------------------------------------')
 
 def Trimming(args):
     #trimming option Note- this is for simple trimming only, not linked adapter trimming
     
+    #--trim needs to be specified in the command line. Otherwise it skips this step entirely
     if args.trim is False:
         print('Proceeding without trimming...')
         return
@@ -105,20 +92,34 @@ def Trimming(args):
         if not os.path.exists('SRA'):
             print('The SRA directory does not exist. Exiting.')
             exit(1)
-            
-        for file in os.listdir('SRA'):
-                if file.endswith('_1.fastq') or file.endswith('_1.fastq.gz'):
+        #The structure of SRA files are different between RNA-Seq and GRO-Seq. RNAS has two passes, while gro-seq only has one.
+        #so the files need to be trimmed differently to account for this.    
+        if args.mode == "rnaseq":
+            for file in os.listdir('SRA'):
+                #removed the or file.endswith("_pass_1.fastq.gz as none of the files in SRA are processed like this")
+                if file.endswith("_pass_1.fastq"):
+                    #the path of the SRA, folder and the file are joined together. E.G /home/diss/SRA + SRR441391_pass_1.fastq
                     input_file = os.path.join('SRA', file)
-                    input_file_2 = os.path.join('SRA', file.replace('_1.fastq', '_2.fastq').replace('_1.fastq.gz', '_2.fastq.gz'))
-                    output_file = os.path.join('Trimmed_data', file.replace('.fastq', '_trimmed.fastq').replace('.fastq.gz', '_trimmed.fastq.gz'))
-                    output_file_2 = os.path.join('Trimmed_data', file.replace('_1.fastq', '_2_trimmed.fastq').replace('_1.fastq.gz', '_2_trimmed.fastq.gz'))
+                    input_file_2 = os.path.join('SRA', file.replace('_pass_1.fastq', '_pass_2.fastq'))
+                    output_file = os.path.join('Trimmed_data', file.replace('_pass_1.fastq', '_pass_2_trimmed.fastq'))
+                    output_file_2 = os.path.join('Trimmed_data', file.replace('_pass_1.fastq', '_pass_2_trimmed.fastq'))
 
                     cutadapt = subprocess.run('cutadapt -a ' + args.adapter1 + ' -A ' + args.adapter2 + ' -o ' + output_file + ' -p ' + output_file_2 + '' + input_file + '' + input_file_2, shell=True)
                     if cutadapt.returncode !=0:
                         print('Error occured')
+                        exit(1)
                     else: 
                         print('The trimming is now done..')
-                        print('-----------------')
+        else:
+            for file in os.listdir("SRA"):
+                if file.endswith("_pass.fastq"):
+                    input_file = os.path.join('SRA', file)
+                    output_file = os.path.join('Trimmed_data', file.replace('_pass.fastq', '_pass_trimmed.fastq'))
+                    #the cutadapt command is shortened for gro-seq as it only requires one input, output and adapter. The -A for second adapter
+                    # and -p "paired" flags are removed.
+                    cutadapt = subprocess.run('cutadapt -a ' + args.adapter1 + ' -o ' + output_file + ' ' + input_file , shell=True)
+
+
                         
 def Multiqc(args):
     #this method takes all of the FastQC files and combines them into a MultiQC for easier interpretation
